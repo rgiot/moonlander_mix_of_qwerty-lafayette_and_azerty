c4e7b8446d118e25b4056a7cb1b32d82e071a220
rgiot <giot.romain@gmail.com>
2021-01-21 09:23:54 +0100

    Some led fixes

--- config.h
+++ config.h
@@ -3,12 +3,15 @@
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
 #define ORYX_CONFIGURATOR
+
+
 #undef RGB_DISABLE_TIMEOUT
 #define RGB_DISABLE_TIMEOUT 300000
+#define RGB_MATRIX_STARTUP_SPD 60
 
 #define NO_AUTO_SHIFT_TAB
 #define NO_AUTO_SHIFT_SPECIAL
 #define NO_AUTO_SHIFT_ALPHA
-#define RGB_MATRIX_STARTUP_SPD 60
+
 
 #define UNICODE_SELECTED_MODES UC_LNX, UC_WINC
